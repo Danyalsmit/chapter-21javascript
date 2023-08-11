6bb5bf826b69afe7555fe0c1e49fab24f76fd31b
Danyalsmit <121617130+Danyalsmit@users.noreply.github.com>
2023-08-11 12:58:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// cha[ter no 21]                     new alert sweetalert
+// swal({
+//     title: "Good job!",
+//     text: "You clicked the button!",
+//     icon: "success",
+//     button: "Aww yiss!",
+//   });
+//  var dk = "Khan"                       CONVERT TO UPPER CASE
+ 
+//  console.log(dk.toUpperCase());
+  
+//  var dk = "DANYAL"                    CONVERT TO LOWER CASE
+ 
+//  console.log(dk.toLowerCase());
+
+// var dk= "pakistan saylani zindabad saylani zindabad"
+// console .log (dk.lastIndexOf("saylani"))
